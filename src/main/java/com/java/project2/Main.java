9fe8e7ee56e253f66f1bd505abd5a3021f41faf0
Agata <agata.weltrowska@outlook.com>
2022-01-30 20:37:21 +0100

    ostatni

--- src/main/java/com/java/project2/Main.java
+++ src/main/java/com/java/project2/Main.java
@@ -2,6 +2,16 @@ package com.java.project2;
 
 public class Main {
     public static void main(String[] args) {
+        Person person1 = new Person("Kornel",1992);
+        Person person2 = new Person("Andrzej",1984);
+        Person person3 = new Person("Mariola",1980);
 
+
+        System.out.println(person1.getName());
+        System.out.println(person1.getYearOfBirth());
+        person1.tellAboutYourself();
+        person2.setName("Ziomek");
+        person2.tellAboutYourself();
+        person3.tellAboutYourself();
     }
 }
--- src/main/java/com/java/project2/Person.java
+++ src/main/java/com/java/project2/Person.java
@@ -1,7 +1,30 @@
 package com.java.project2;
 
 public class Person {
-    public static void main(String[] args) {
+    private String name;
+    private int yearOfBirth;
 
+    public Person(String name, int yearOfBirth) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public Person() {
+    }
+
+    public void tellAboutYourself() {
+        System.out.println("Hi! My name is " + name + " and I'm " + (2022-yearOfBirth) + " old.");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
     }
 }
